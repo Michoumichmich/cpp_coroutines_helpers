6b88e1bb8884d6e4690f46723e49d58b61039e75
Michel MIGDAL <michel.migdal@ensiie.fr>
2022-01-07 19:38:14 +0100

    Comments

--- sycl_examples/main.cpp
+++ sycl_examples/main.cpp
@@ -4,15 +4,17 @@
  * Doing more work in parallel on a single device/several devices with nice semantics. 
  * Otherwise you would have to use vectors of states and sycl::buffer or sycl::events + states + USM. 
  * Demo using hipSYCL: https://github.com/Michoumichmich/hipSYCL/commit/5cf4359b1abc9cf48ac37fd2f90c1d91a65e1a89
+ * Tested on gcc 11.2.0
  */
 
 #include <iostream>
 #include <deque>
 #include <coro>
 #include <sycl/sycl.hpp>
-
+ 
+/* See <coro> for more */ 
 template<typename T = void>
-using sycl_task = single_task<T, true, true>;
+using sycl_task = single_task<T, true, true>; // begin coroutine immediately + exceptions
 
 template <typename T = void>
 using sycl_pipeline = std::deque<sycl_task<T>>;
@@ -39,8 +41,7 @@ auto advance_pipeline(sycl_pipeline<T> &work_pipeline) -> std::optional<T> {
  * sycl_task<T> runs until the first co_await/return and propagates exceptions
  * When co_await returns, the event has finished.
  */
-template<typename T>
-auto sycl_task_flow_example(T in, sycl::queue q) -> sycl_task<T> {
+auto sycl_task_flow_example(unsigned in, sycl::queue q) -> sycl_task<unsigned> {
     auto *dev_ptr = sycl::malloc_device<unsigned>(1U, q);
     /* Set up the context of the computation, heavy */
     co_await q.copy(&in, dev_ptr, 1U);
@@ -56,10 +57,11 @@ auto sycl_task_flow_example(T in, sycl::queue q) -> sycl_task<T> {
 int main() {
     /* Something where we will store the coroutines */ 
     auto work_pipeline = sycl_pipeline<unsigned>{};
+    auto q = sycl::queue{};
 
-    /* Launching 20 jobs/streams */
-    for (auto i : range(0U, 20U)) {
-        work_pipeline.emplace_back(sycl_task_flow_example(i, sycl::queue{}));
+    /* Launching 20 parallel jobs on q */
+    for (auto i : range(0, 20)) {
+        work_pipeline.emplace_back(sycl_task_flow_example(i, q));
         if (auto result = advance_pipeline(work_pipeline)) {
             std::cout << "Result: " << *result << ", pipeline depth" << work_pipeline.size() << std::endl; /* Process the result, some heavy computation */
         }
